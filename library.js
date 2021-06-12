6f05e7089c9267561d7717ab370f638a2d7f24cb
Anna <CodeWithAnna@gmail.com>
2021-06-12 15:34:16 +0530

    initialcommit

---
+++ library.js
@@ -0,0 +1,27 @@
+console.log("hello");
+
+//constructor
+function Book(name, author, type){
+this.name = name;
+this.author = author;
+this.type = type;
+}
+
+//display constructor displays the values 
+function Display(){
+
+}
+
+//Add method to display prototype
+
+
+
+// ??Add submit even listner to form libraryForm/
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(){
+    else.preventDefault();
+    console.log('You have Submitted Library Form');
+}
